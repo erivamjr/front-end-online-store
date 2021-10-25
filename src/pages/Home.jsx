3024506222cef3c9bc6d29ad58eab17ece751501
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-10-25 18:17:45 -0300

    [main-group-12] Requisito 04: Liste as categorias de produtos disponíveis via API na página principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+    this.fetchCategories = this.fetchCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  async fetchCategories() {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
+        <div>
+          <ul>
+            {categories.map(({ id, name }) => (
+              <li key={ id } data-testid="category">
+                { name }
+              </li>
+            ))}
+          </ul>
+        </div>
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
